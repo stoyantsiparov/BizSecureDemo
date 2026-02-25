37813ed6646b84c2f861e44c6cdda82d1cacd837
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-02-25 16:43:14 +0200

    Add rate limiting to login route to prevent brute-force attacks
    Implemented a fixed window rate limiter for the "Account/Login" route, allowing up to 5 login attempts per minute with no queuing. Integrated the rate limiter middleware into the request pipeline to enhance security. Also performed minor code cleanup and removed obsolete comments.

--- BizSecureDemo_22180022/Program.cs
+++ BizSecureDemo_22180022/Program.cs
@@ -1,6 +1,7 @@
 using BizSecureDemo_22180022.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,9 +24,18 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     });
 
 builder.Services.AddAuthorization();
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("login", o =>
+    {
+        o.PermitLimit = 5; // 5 attempts
+        o.Window = TimeSpan.FromMinutes(1);
+        o.QueueLimit = 0;
+    });
+});
 
 var app = builder.Build();
-// създаваме DB автоматично (без миграции)
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -34,11 +44,16 @@ using (var scope = app.Services.CreateScope())
 
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseRateLimiter();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+        name: "login",
+        pattern: "Account/Login",
+        defaults: new { controller = "Account", action = "Login" })
+    .RequireRateLimiting("login");
 app.Run();
